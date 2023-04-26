91d819e14c95f3afaaae753529635299c27bceca
ishoshot <ishoshot@gmail.com>
2023-04-26 17:40:18 +0100

    Removed Validator from AccountEnquiry

--- api/AccountEnquiry.js
+++ api/AccountEnquiry.js
@@ -1,16 +1,21 @@
 const { log } = require('../utils/log')
-const Validator = require('../utils/Validator')
-const validator = new Validator()
 const uuidv4 = require('uuid')
 
 class AccountEnquiry {
   accountBalance(data, req, res) {
     const uniqueID = uuidv4.v4()
     const { AccountNumber } = data;
-    const valid = validator.accountBalance(data, req, res)
-    if (!valid) {
-      return;
+    if (!AccountNumber) {
+      return res.status(200).json({
+        code: '09',
+        message: 'Sorry! I believe you are trying to get the balance of an account. Kindly rephrase your request and ensure an account number is provided.',
+        otherMessage: ''
+      });
     }
+    // const valid = validator.accountBalance(data, req, res)
+    // if (!valid) {
+    //   return;
+    // }
     // Call log on Request
     log('Incoming', uniqueID, data)
     // Call Bank's API
